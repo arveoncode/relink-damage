aa5e9b9b379afbc979708dac885a6f81a84a2144
arveoncode <arvinaenriquez@gmail.com>
2024-04-19 05:28:38 +0800

    fixed page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <div className="px-8 flex justify-between">
         <BuildSlotTabs />
         <div className="flex gap-4">
-          <ExportDialog />
+          <ExportDialog defaultTab="link" />
           <ImportDialog />
         </div>
       </div>

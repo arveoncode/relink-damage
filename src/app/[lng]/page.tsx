e09de74ff0dc2de4a985659b5e45c43fb73583aa
arveoncode <arvinaenriquez@gmail.com>
2024-05-02 04:46:00 +0800

    changed suspense on root page

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -17,37 +17,36 @@ import { BannerNotif } from "../(components)/BannerNotif";
 export default function Home({ params: { lng } }: { params: { lng: string } }) {
   if (languages.indexOf(lng as Language) < 0) lng = fallbackLng;
   return (
-    <Suspense>
-      <main className="flex flex-col relative">
-        <BannerNotif />
-        <div className="px-8 flex justify-between">
-          <BuildSlotTabs />
-          <div className="flex gap-4">
-            <ExportDialog defaultTab="link" />
-            <ImportDialog />
-          </div>
+    <main className="flex flex-col relative">
+      <BannerNotif />
+      <div className="px-8 flex justify-between">
+        <BuildSlotTabs />
+        <div className="flex gap-4">
+          <ExportDialog defaultTab="link" />
+          <ImportDialog />
         </div>
-        <div className="grid grid-cols-10 gap-4 p-8">
-          <div className="flex flex-col gap-4 col-span-2">
-            <CharacterPicker />
-            <Overmasteries />
-            <OtherInputs />
-          </div>
-          <div className="col-span-4 flex gap-4 flex-col">
-            <Sigils />
-            <Weapon />
-          </div>
-          <div className="col-span-4 flex flex-col gap-4">
-            <Traits />
-            <Stats />
-          </div>
+      </div>
+      <div className="grid grid-cols-10 gap-4 p-8">
+        <div className="flex flex-col gap-4 col-span-2">
+          <CharacterPicker />
+          <Overmasteries />
+          <OtherInputs />
         </div>
-        <div className="p-8 bg-white">
-          <SkillsTable />
+        <div className="col-span-4 flex gap-4 flex-col">
+          <Sigils />
+          <Weapon />
         </div>
-
+        <div className="col-span-4 flex flex-col gap-4">
+          <Traits />
+          <Stats />
+        </div>
+      </div>
+      <div className="p-8 bg-white">
+        <SkillsTable />
+      </div>
+      <Suspense>
         <LogsImporter />
-      </main>
-    </Suspense>
+      </Suspense>
+    </main>
   );
 }

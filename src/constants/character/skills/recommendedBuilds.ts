43a6f59b3d9a3c3db67f180149e6401cac16782c
アルビオンの夢 <arvinaenriquez@gmail.com>
2024-05-07 14:03:22 +0800

    Feat/recommended builds (#30)
    * initial responsiveness, didn't expect users to access from mobile
    
    * more mobile responsiveness stuff
    
    * changed image export layout
    
    * forgot to change package.json version lmao
    
    * fixed Combo Finisher DMG not providing finisher bonus
    
    * initial recommended-builds feature foundation
    
    * added foundation for recommended builds and default tweyen build

---
+++ src/constants/character/skills/recommendedBuilds.ts
@@ -0,0 +1,104 @@
+import { Equipment } from "@/types/app.types";
+import { Character } from "@/types/character.types";
+
+interface RecommendedBuildPage {
+  character: Character;
+  builds: SampleBuild[];
+}
+
+interface SampleBuild extends Equipment {
+  name: string;
+}
+
+export const recommendedBuilds: RecommendedBuildPage[] = [
+  { character: "Io", builds: [] },
+  { character: "Zeta", builds: [] },
+  { character: "Captain", builds: [] },
+  { character: "Narmaya", builds: [] },
+  { character: "Rosetta", builds: [] },
+  { character: "Cagliostro", builds: [] },
+  { character: "Ferry", builds: [] },
+  { character: "Lancelot", builds: [] },
+  { character: "Rackam", builds: [] },
+  { character: "Vaseraga", builds: [] },
+  { character: "Vane", builds: [] },
+  { character: "Id", builds: [] },
+  { character: "Charlotta", builds: [] },
+  { character: "Ghandagoza", builds: [] },
+  { character: "Percival", builds: [] },
+  { character: "Katalina", builds: [] },
+  { character: "Eugen", builds: [] },
+  { character: "Yodarha", builds: [] },
+  {
+    character: "Tweyen",
+    builds: [
+      {
+        name: "Default Tweyen",
+        selectedCharacter: "Tweyen",
+        sigilsEquipped: [
+          { sigil1: "Alpha", sigil2: "DMG Cap", level: 15 },
+          { sigil1: "Alpha", sigil2: "DMG Cap", level: 15 },
+          { sigil1: "Gamma", sigil2: "DMG Cap", level: 15 },
+          { sigil1: "Gamma", sigil2: "DMG Cap", level: 15 },
+          {
+            sigil1: "Supplementary DMG",
+            sigil2: "Nimble Onslaught",
+            level: 15,
+          },
+          {
+            sigil1: "Supplementary DMG",
+            sigil2: "Quick Cooldown",
+            level: 15,
+          },
+          {
+            sigil1: "Quick Charge",
+            sigil2: "Cascade",
+            level: 15,
+          },
+          {
+            sigil1: "Critical Hit Rate",
+            sigil2: "Stamina",
+            level: 15,
+          },
+          {
+            sigil1: "War Elemental",
+            sigil2: "None",
+            level: 15,
+          },
+          {
+            sigil1: "Awakening",
+            sigil2: "None",
+            level: 15,
+          },
+          {
+            sigil1: "Warpath",
+            sigil2: "None",
+            level: 15,
+          },
+          {
+            sigil1: "Boundary",
+            sigil2: "None",
+            level: 15,
+          },
+        ],
+        overmasteries: {
+          attack: 1000,
+          normalDamageCapUp: 0.2,
+          skillDamageCapUp: 0.2,
+          sbaDamageCapUp: 0,
+          sbaDamageUp: 0,
+          skillDamageUp: 0,
+          critHitRate: 0.2,
+        },
+        weaponImbues: [
+          { traitName: "Stun Power", level: 10 },
+          { traitName: "Concentrated Fire", level: 7 },
+          { traitName: "Cascade", level: 5 },
+        ],
+        isMaxAwakening: true,
+        isTerminus: false,
+      },
+    ],
+  },
+  { character: "Seofon", builds: [] },
+];
